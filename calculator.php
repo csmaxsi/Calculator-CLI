711164ec1c7abd24dce864460fea62b6a3f13c6f
Chathura Sankalpa <87167914+csmaxsi@users.noreply.github.com>
2024-06-20 00:29:13 +0530

    calculator_encap.php

--- calculator.php
+++ calculator.php
@@ -1,90 +1,146 @@
 <?php
-class Calculator
+interface Shape
 {
-    protected function calCircle(): void
-    {
-        $radius = $this->getAnswer("Enter Radius: ");
-        $area = 2*(22/7)*$radius;
-        $perimeter = 2*(22/7)*($radius);
-        echo "\nArea is : $area";
-        echo "\nperimeter is : $perimeter\n";  
-    }
-    
-    protected function calRectangle() : void
-    {
-        $width = $this->getAnswer("Enter width: ");
-        $height = $this->getAnswer("Enter height: ");
-        $area = $width*$height;
-        $perimeter = 2*($width+$height);
-        echo "\nArea is : $area";
-        echo "\nperimeter is : $perimeter\n";
-    }
-    
-    protected function calSquare() : void
-    {
-        $length= $this->getAnswer("Enter length: ");
-        $area = $length*$length;
-        $perimeter = 4*($length);
-        echo "\nArea is : $area";
-        echo "\nperimeter is : $perimeter\n";
-    }
+     public function calculateArea(): float;
+     public function calculatePerimeter() : float;
 }
 
+class Circle implements Shape
+{
+    private float $radius;
+    
+    public function __construct(float $radius)
+    {
+        $this->radius = $radius;
+    }
+    
+    public function calculateArea(): float
+    {
+        
+        return (22/7)*$this->radius*$this->radius;
+        
+    }
+    
+    public function calculatePerimeter(): float
+    {
+       
+        return 2*(22/7)*$this->radius;
+    }
+    
+    
+}
 
-class CalCommandPrompt extends Calculator
+class Rectangle implements Shape
+{
+    private float $height;
+    private float $width;
+    
+    public function __construct(float $height , float $width) 
+    {
+        $this->height = $height;
+        $this->width = $width;
+    }
+    
+    public function calculateArea(): float
+    {
+        
+        return $this->height*$this->width;
+        
+    }
+    
+    public function calculatePerimeter(): float
+    {
+       
+        return 2*($this->height+$this->width);
+    }
+    
+    
+}
+
+class Square implements Shape
+{
+    private float $length;
+    
+    public function __construct(float $length)
+    {
+        $this->length = $length;
+    }
+    
+    public function calculateArea(): float
+    {
+        return $this->length*$this->length;
+    }
+    
+    public function calculatePerimeter(): float
+    {
+        return 4*$this->length;
+    }
+    
+}
+
+class CalCommandPrompt
 {   
-
-    public function startFlow(): void
+    
+    public function showMessage(string $message) : void
+    {
+         echo $message;
+    }
+    
+    public function showMainMessage() : void
+    {
+        $this->showMessage("\nchoose an option : \n\n1. Calculate Circle \n2. Calculate Rectangle \n3. Calculate Square \n4. Exit\n");
+    }
+    
+    public function getAnswer(string $question):string
+    {
+        return readline($question);
+    }
+    
+    public function startFlow() : void
     {
         $this->showMainMessage();
-        $answer = $this->getAnswer("");
+        $ans = $this->getAnswer("");
         
-        
-        switch($answer)
-        {   
+        switch($ans)
+        {
             case 1:
-                $this->calCircle();
-                break;
                 
+                $radius = $this->getAnswer("Enter the radius :");
+                $circle = new Circle($radius);
+                $setA = $circle->calculateArea();
+                $this->showMessage("Area is :$setA");
+                $setP = $circle->calculatePerimeter();
+                $this->showMessage("\nPerimeter is :$setP\n");
+                break;
             case 2:
-                $this->calRectangle();
+                $height = $this->getAnswer("Enter the height :");
+                $width = $this->getAnswer("Enter the width :");
+                $rectangle = new Rectangle($height , $width);
+                $setA = $rectangle->calculateArea();
+                $setP = $rectangle->calculatePerimeter();
+                $this->showMessage("Area is :$setA");
+                $this->showMessage("\nPerimeter is :$setP\n");
                 break;
-                
             case 3:
-                $this->calSquare();
+                $length = $this->getAnswer("Enter length :");
+                $square = new Square($length);
+                $setA = $square->calculateArea();
+                $setP = $square->calculatePerimeter();
+                $this->showMessage("Area is :$setA");
+                $this->showMessage("\nPerimeter is :$setP\n");
                 break;
             case 4:
                 exit;
             default:
-                $this->showMessage("Unsupported input");
+                $this->showMessage("Unsupported Output....Try again !.\n");
                 break;
                 
         }
-        
         $this->startFlow();
-    }
-    
-    
-    
-    public function showMessage(string $message): void
-    {
-        echo $message;
-    }
-    
-    public function getAnswer(string $question): string 
-    {
-        $answer=readline($question);
-        return $answer;
-    }
-    
-    public function showMainMessage(): void
-    {
-        $this->showMessage("\nchoose an option : \n\n1. Calculate Circle \n2. Calculate Rectangle \n3. Calculate Square \n4. Exit\n");
     }
 }
 
-
-$cal = new CalCommandPrompt();
-$cal->startFlow();
+$calCommandPrompt = new CalCommandPrompt();
+$calCommandPrompt->startFlow();
 
 ?>
